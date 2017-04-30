3306bf2271356786458eeb6801a4b1c8d5972ef7
Buity <ornellia.buity@devinci.fr>
2017-04-30 16:24:39 +0200

    Update scritp.js

--- js/scritp.js
+++ js/scritp.js
@@ -1,10 +1,16 @@
-var xmlhttp = new XMLHttpRequest();
+function Requete(url)
+{
+	var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
         document.getElementById("demo").innerHTML = jsonPretty;
     }
+}
+xmlhttp.open("GET", "url", true);
+xmlhttp.send();
+
 };
 
 
