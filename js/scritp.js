af4394590f8c7f636468648e44863394916df571
Buity <ornellia.buity@devinci.fr>
2017-04-30 16:39:36 +0200

    Update scritp.js

--- js/scritp.js
+++ js/scritp.js
@@ -1,16 +1,18 @@
-function Requete(url)
+function homePageLoading(url,elementID) 
 {
-	var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = this.responseText;
-        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo").innerHTML = jsonPretty;
-    }
-}
-xmlhttp.open("GET", "url", true);
-xmlhttp.send();
-
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function()
+		{
+		    if (this.readyState == 4 && this.status == 200) 
+		    {
+			var myObj = this.responseText;
+			var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+			document.getElementById("demo").innerHTML = jsonPretty;
+		    }
+		}
+	
+	xmlhttp.open("GET", "url", true);
+	xmlhttp.send();
 }
 
 
